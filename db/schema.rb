a4f3b9a5ec23f42ff6a97a107acb1986158bf9ee
EC2 Default User <ec2-user@ip-172-31-16-112.ap-southeast-1.compute.internal>
2019-01-13 05:39:28 +0000

    add category

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_150854) do
+ActiveRecord::Schema.define(version: 2019_01_13_044501) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
