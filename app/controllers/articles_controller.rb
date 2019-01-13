89bafeefbe96f399926174f4d9135b76936f109d
EC2 Default User <ec2-user@ip-172-31-16-112.ap-southeast-1.compute.internal>
2019-01-13 06:21:54 +0000

    add address

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -80,6 +80,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:user_id, :content, :category_id)
+      params.require(:article).permit(:user_id, :content, :category_id, :address)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_13_044501) do
+ActiveRecord::Schema.define(version: 2019_01_13_061557) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_01_13_044501) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.string "address"
   end
 
   create_table "categories", force: :cascade do |t|
